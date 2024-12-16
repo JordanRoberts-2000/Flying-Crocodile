d5ce89434e739d915e92562a4de0a954fe8441ae
Jordan Roberts <jordanroberts333@icloud.com>
2024-12-16 03:44:35 +0000

    refactor: entry model contains method to create an index for roots

--- server/src/models/entry/mod.rs
+++ server/src/models/entry/mod.rs
@@ -18,7 +18,7 @@ pub struct Entry {
 }
 
 impl Entry {
-    pub fn create_root(connection: &mut PgConnection, root_name: &str) -> Result<Self, String> {
+    pub fn create_root(conn: &mut PgConnection, root_name: &str) -> Result<Self, String> {
         let new_entry = NewEntry {
             title: root_name.to_string(),
             parent_id: None,
@@ -29,12 +29,26 @@ impl Entry {
         diesel::insert_into(entries::table)
             .values(&new_entry)
             .returning(entries::all_columns)
-            .get_result::<Entry>(connection)
+            .get_result::<Entry>(conn)
             .map_err(|e| {
                 format!(
                     "Error inserting root entry `{}` into the database: {}",
                     root_name, e
                 )
             })
+    }
+
+    pub fn create_index(conn: &mut PgConnection, root_id: i32) -> Result<String, String> {
+        let index_name = format!("idx_entries_by_root_id_{}", root_id);
+        let create_index_query = format!(
+            "CREATE INDEX {} ON entries (parent_id) WHERE root_id = {};",
+            index_name, root_id
+        );
+
+        diesel::sql_query(create_index_query)
+            .execute(conn)
+            .map_err(|e| format!("Failed to create index for root_id {}: {}", root_id, e))?;
+
+        Ok(index_name)
     }
 }
--- server/src/services/root/create_root.rs
+++ server/src/services/root/create_root.rs
@@ -1,19 +1,18 @@
 use crate::{models::Entry, state::AppState};
-use diesel::prelude::*;
 use log::debug;
 
 use super::RootService;
 
 impl RootService {
     pub fn create_root(app_state: &AppState, root_name: &str) -> Result<Entry, String> {
-        let mut connection = app_state.db_pool.get().map_err(|e| {
+        let mut conn = app_state.db_pool.get().map_err(|e| {
             format!(
                 "Failed to get DB connection from pool while creating entry `{}`: {}",
                 root_name, e
             )
         })?;
 
-        let inserted_entry = Entry::create_root(&mut connection, root_name)?;
+        let inserted_entry = Entry::create_root(&mut conn, root_name)?;
 
         Self::add_to_cache(&app_state, &inserted_entry)?;
 
@@ -22,20 +21,7 @@ impl RootService {
             root_name, inserted_entry.id
         );
 
-        let index_name = format!("idx_entries_by_root_id_{}", inserted_entry.id);
-        let create_index_query = format!(
-            "CREATE INDEX {} ON entries (parent_id) WHERE root_id = {};",
-            index_name, inserted_entry.id
-        );
-
-        diesel::sql_query(create_index_query)
-            .execute(&mut connection)
-            .map_err(|e| {
-                format!(
-                    "Failed to create index for root_id {}: {}",
-                    inserted_entry.id, e
-                )
-            })?;
+        let index_name = Entry::create_index(&mut conn, inserted_entry.id)?;
 
         debug!(
             "Index `{}` created successfully for root ID {}.",
